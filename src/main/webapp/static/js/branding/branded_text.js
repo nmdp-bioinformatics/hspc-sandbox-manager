4c259c68170fbdcd27480c203208c82414e506e4
Travis <travis@Travis-MacBook-Pro.local>
2017-05-06 21:48:08 -0600

    FHIR v3.0.1

--- src/main/webapp/static/js/branding/branded_text.js
+++ src/main/webapp/static/js/branding/branded_text.js
@@ -20,7 +20,7 @@ angular.module("sandManApp.branding", [], ["$provide", function($provide) {
                     {version: "1", name: "FHIR DSTU 2 (v1.0.2)", fhirVersion: "1.0.2", altName: "FHIR v1.0.2", canCreate: true},
                     {version: "2", name: "FHIR DSTU 2 (v1.6.0)", fhirVersion: "1.6.0", altName: "FHIR v1.6", canCreate: false},
                     {version: "3", name: "FHIR STU 3 (v1.8.0)", fhirVersion: "1.8.0", altName: "FHIR v1.8", canCreate: true},
-                    {version: "4", name: "FHIR STU 3 (v1.9.0)", fhirVersion: "1.9.0", altName: "FHIR v1.9", canCreate: true}
+                    {version: "4", name: "FHIR STU 3 (v3.0.1)", fhirVersion: "3.0.1", altName: "FHIR v3.0.1", canCreate: true}
                 ],
                 sandboxDescription: {
                     title: "What is a sandbox?",
