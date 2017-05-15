136ea7856bd1963f79217ef2c2da6f1600d05463
Travis <travis@Travis-MacBook-Pro.local>
2017-05-15 13:41:58 -0600

    Properties updates

--- src/main/java/org/hspconsortium/sandboxmanager/SandboxManagerApplication.java
+++ src/main/java/org/hspconsortium/sandboxmanager/SandboxManagerApplication.java
@@ -1,14 +1,11 @@
 package org.hspconsortium.sandboxmanager;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.tuckey.web.filters.urlrewrite.UrlRewriteFilter;
 
 @SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class})
 @ComponentScan({"org.hspconsortium"})
@@ -23,8 +20,4 @@ public class SandboxManagerApplication extends SpringBootServletInitializer {
         ApplicationContext ctx = SpringApplication.run(SandboxManagerApplication.class, args);
     }
 
-    @Bean
-    public UrlRewriteFilter getUrlRewriteFilter() {
-        return new UrlRewriteFilter();
-    }
 }
