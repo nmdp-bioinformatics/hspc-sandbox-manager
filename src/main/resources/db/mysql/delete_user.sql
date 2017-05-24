6ef360722c8cd375d7f58881815987eb5662edef
Travis <travis@Travis-MacBook-Pro.local>
2017-05-24 12:38:28 -0600

    fixed error in admin.js due to no trailing "/"

--- src/main/resources/db/mysql/delete_user.sql
+++ src/main/resources/db/mysql/delete_user.sql
@@ -5,8 +5,7 @@
     * personas
     * lunch contexts
  */
-
-SET @user_id := 0;
+SET @user_id := 62;
 
 /* activity log */
 DELETE FROM sandbox_activity_log
@@ -19,17 +18,24 @@ WHERE user_id = @user_id;
 /* Sandboxes */
 DELETE FROM user_sandbox
 WHERE user_id = @user_id;
+
 DELETE FROM sandbox_user_roles
 WHERE user_roles IN (SELECT id
                      FROM user_role
                      WHERE user_id = @user_id);
+
 DELETE FROM user_role
 WHERE user_id = @user_id;
-
 
 DELETE FROM sandbox
 WHERE created_by_id = @user_id;
 
+DELETE FROM user_terms_of_use_acceptance
+WHERE user_id = @user_id;
+
+DELETE FROM sandbox_invite
+WHERE invitee_id = @user_id
+      OR invited_by_id = @user_id;
 
 /* finally, the actual user */
 DELETE FROM user
