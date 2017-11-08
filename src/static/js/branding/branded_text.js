fdc14f545b8795a6b70f5bcb95f16a6890cf349d
Eric Lineback <eric@interopion.com>
2017-11-08 12:51:20 -0700

    Made index 5 visible.

--- src/static/js/branding/branded_text.js
+++ src/static/js/branding/branded_text.js
@@ -23,7 +23,7 @@ angular.module("sandManApp.branding", [], ["$provide", function($provide) {
                     {index: "2", name: "FHIR DSTU 2 (v1.6.0)", fhirVersion: "1.6.0", fhirTag: "1_0_2", altName: "FHIR v1.6", canCreate: false, supportsDataSets: false},
                     {index: "3", name: "FHIR STU 3 (v1.8.0)", fhirVersion: "1.8.0", fhirTag: "1_8_0", altName: "FHIR v1.8", canCreate: false, supportsDataSets: false},
                     {index: "4", name: "FHIR STU 3 (v3.0.1)", fhirVersion: "3.0.1", fhirTag: "3_0_1", altName: "FHIR v3.0.1", canCreate: false, supportsDataSets: true},
-                    {index: "5", name: "FHIR DSTU 2 (v1.6.0)", fhirVersion: "1.6.0", fhirTag: "1_0_2", altName: "FHIR v1.6", canCreate: false, supportsDataSets: false},
+                    {index: "5", name: "FHIR DSTU 2 (v1.6.0)", fhirVersion: "1.6.0", fhirTag: "1_0_2", altName: "FHIR v1.6", canCreate: true, supportsDataSets: false},
                     {index: "6", name: "FHIR STU 3 (v3.0.1)", fhirVersion: "3.0.1", fhirTag: "3_0_1", altName: "FHIR v3.0.1", canCreate: true, supportsDataSets: true}
                 ],
                 sandboxDescription: {
