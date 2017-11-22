1eb11148fa7761a4e28b9af12d903c49290c2506
Eric Lineback <eric@interopion.com>
2017-11-22 14:53:20 -0700

    Change server initial finder

--- Server.js
+++ Server.js
@@ -4,7 +4,7 @@ var path    = require("path");
 
 app.use(express.static(path.join(__dirname + '/src')));
  
-app.get('*',function(req,res){
+app.get('/',function(req,res){
   res.sendFile(__dirname+'/src/index.html');
 });
 
