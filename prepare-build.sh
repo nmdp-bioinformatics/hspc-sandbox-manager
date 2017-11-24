7fd7ea38da5b71a912d99751b339e0221bc7a930
Travis Cummings <travis@interopion.com>
2017-11-24 13:52:36 -0700

    clone depth 1

--- prepare-build.sh
+++ prepare-build.sh
@@ -27,7 +27,21 @@ cat src/index.html | sed -E -e "s/.js\?r=[0-9.]+/.js\?r=$PROJECT_VERSION/g" 2>&1
 echo 'dynamically fix the container-definitions_prod.json'
 cat container-definitions_prod.json | jq --arg container_name $PROJECT_NAME '.[0].name=$container_name' | jq --arg image_name $IMAGE_NAME '.[0].image=$image_name' | jq --arg container_port $PROJECT_PORT '.[0].portMappings[0].containerPort=($container_port | tonumber)' 2>&1 | tee container-definitions_prod.json
 
+if ! [ -s container-definitions_prod.json ]
+then
+  echo "container-definitions_prod.json is empty!"
+  exit 1
+end
+fi
+
 echo 'dynamically fix the container-definitions_test.json'
 cat container-definitions_test.json | jq --arg container_name $PROJECT_NAME '.[0].name=$container_name' | jq --arg image_name $IMAGE_NAME '.[0].image=$image_name' | jq --arg container_port $PROJECT_PORT '.[0].portMappings[0].containerPort=($container_port | tonumber)' 2>&1 | tee container-definitions_test.json
+
+if ! [ -s container-definitions_test.json ]
+then
+  echo "container-definitions_test.json is empty!"
+  exit 1
+end
+fi
 
 echo 'finished prepare_build.sh'
