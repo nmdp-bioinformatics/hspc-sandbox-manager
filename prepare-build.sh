96c7eedb71e4928ad0c42be930793e42331a7da9
Travis Cummings <travis@interopion.com>
2017-11-24 14:02:00 -0700

    error handling

--- prepare-build.sh
+++ prepare-build.sh
@@ -27,7 +27,7 @@ cat src/index.html | sed -E -e "s/.js\?r=[0-9.]+/.js\?r=$PROJECT_VERSION/g" 2>&1
 echo 'dynamically fix the container-definitions_prod.json'
 cat container-definitions_prod.json | jq --arg container_name $PROJECT_NAME '.[0].name=$container_name' 2>&1 | tee container-definitions_prod.json
 cat container-definitions_prod.json | jq --arg image_name $IMAGE_NAME '.[0].image=$image_name' 2>&1 | tee container-definitions_prod.json
-cat container-definitions_prod.json | jq --arg container_port $PROJECT_PORT '.[0].name=$container_port' 2>&1 | tee container-definitions_prod.json
+cat container-definitions_prod.json | jq --arg container_port $PROJECT_PORT '.[0].portMappings[0].containerPort=($container_port | tonumber)' 2>&1 | tee container-definitions_prod.json
 
 if ! [ -s container-definitions_prod.json ]
 then
@@ -39,7 +39,7 @@ fi
 echo 'dynamically fix the container-definitions_test.json'
 cat container-definitions_test.json | jq --arg container_name $PROJECT_NAME '.[0].name=$container_name' 2>&1 | tee container-definitions_test.json
 cat container-definitions_test.json | jq --arg image_name $IMAGE_NAME '.[0].image=$image_name' 2>&1 | tee container-definitions_test.json
-cat container-definitions_test.json | jq --arg container_port $PROJECT_PORT '.[0].name=$container_port' 2>&1 | tee container-definitions_test.json
+cat container-definitions_test.json | jq --arg container_port $PROJECT_PORT '.[0].portMappings[0].containerPort=($container_port | tonumber)' 2>&1 | tee container-definitions_test.json
 
 if ! [ -s container-definitions_test.json ]
 then
