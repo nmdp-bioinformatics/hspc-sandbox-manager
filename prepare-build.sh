570f868af267435fa036b269fa8650c1a7f1d683
Travis Cummings <travis@interopion.com>
2017-11-24 13:40:02 -0700

    Handle standard error

--- prepare-build.sh
+++ prepare-build.sh
@@ -20,12 +20,12 @@ export IMAGE_NAME=$PROJECT_REPO/$PROJECT_NAME:$PROJECT_VERSION
 echo "IMAGE_NAME: $IMAGE_NAME"
 
 echo 'dynamically fix the JavaScript references to bypass cache on new deployments'
-cat src/index.html | sed -E -e "s/.js\?r=[0-9.]+/.js\?r=$PROJECT_VERSION/g" | tee src/index.html
+cat src/index.html | sed -E -e "s/.js\?r=[0-9.]+/.js\?r=$PROJECT_VERSION/g" 2>&1 | tee src/index.html
 
 echo 'dynamically fix the container-definitions_prod.json'
-cat container-definitions_prod.json | jq --arg container_name $PROJECT_NAME '.[0].name=$container_name' | jq --arg image_name $IMAGE_NAME '.[0].image=$image_name' | jq --arg container_port $PROJECT_PORT '.[0].portMappings[0].containerPort=($container_port | tonumber)' | tee container-definitions_prod.json
+cat container-definitions_prod.json | jq --arg container_name $PROJECT_NAME '.[0].name=$container_name' | jq --arg image_name $IMAGE_NAME '.[0].image=$image_name' | jq --arg container_port $PROJECT_PORT '.[0].portMappings[0].containerPort=($container_port | tonumber)' 2>&1 | tee container-definitions_prod.json
 
 echo 'dynamically fix the container-definitions_test.json'
-cat container-definitions_test.json | jq --arg container_name $PROJECT_NAME '.[0].name=$container_name' | jq --arg image_name $IMAGE_NAME '.[0].image=$image_name' | jq --arg container_port $PROJECT_PORT '.[0].portMappings[0].containerPort=($container_port | tonumber)' | tee container-definitions_test.json
+cat container-definitions_test.json | jq --arg container_name $PROJECT_NAME '.[0].name=$container_name' | jq --arg image_name $IMAGE_NAME '.[0].image=$image_name' | jq --arg container_port $PROJECT_PORT '.[0].portMappings[0].containerPort=($container_port | tonumber)' 2>&1 | tee container-definitions_test.json
 
 echo 'finished prepare_build.sh'
