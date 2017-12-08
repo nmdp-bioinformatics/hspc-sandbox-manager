9f9748587fc04dd8d984d671894f16685b1bec84
Eric Lineback <eric@interopion.com>
2017-12-08 12:44:46 -0700

    Make sure no caching happens.

--- Server.js
+++ Server.js
@@ -3,10 +3,17 @@ var app     = express();
 var path    = require("path");
 
 app.use(express.static(path.join(__dirname + '/src')));
- 
-app.get('*',function(req,res){
+app.use(function (req, res, next) {
+    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+    res.header('Expires', '-1');
+    res.header('Pragma', 'no-cache');
+    next()
+});
+
+app.get('*', function(req,res){
   res.sendFile(__dirname+'/src/index.html');
 });
 
 app.listen(8080);
+
 
