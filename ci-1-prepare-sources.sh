08ec7d69c415f240f664c63e4fb5d2e22e9a5155
Travis Cummings <travis@interopion.com>
2017-12-29 09:22:55 -0700

    Fix index.html js version replacement

--- ci-1-prepare-sources.sh
+++ ci-1-prepare-sources.sh
@@ -8,7 +8,7 @@ echo "PROJECT_NAME: $PROJECT_NAME"
 echo "CURRENT_ENV: $CURRENT_ENV"
 
 echo "dynamically fix the JavaScript references to bypass cache on new deployments"
-sed -i -e "s/.js\?r=[0-9.]+/.js\?r=$PROJECT_VERSION/g" src/index.html
+sed -E -i -e "s/.js\?r=[0-9.]+(-SNAPSHOT|-latest)/.js\?r=$PROJECT_VERSION/g" src/index.html
 if ! [ -s src/index.html ]
 then
   echo "src/index.html is empty!"
@@ -36,7 +36,7 @@ then
 fi
 
 echo "dynamically configuring the services.js"
-sed -i -e "s/replacethiswithcurrentenvironment/$CURRENT_ENV/g" ./src/static/js/services.js
+sed -i -e "s/replacethiswithcurrentenvironment/$CURRENT_ENV/g" src/static/js/services.js
 
 echo "active_env:"
 cat ./src/static/js/services.js | grep "var active_env ="
