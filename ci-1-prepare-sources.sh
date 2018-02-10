3d547d38feda6b1bad7bc7a3e69d93a7110b850d
Travis Cummings <travis@interopion.com>
2018-02-10 12:31:08 -0700

    new pipeline

--- ci-1-prepare-sources.sh
+++ ci-1-prepare-sources.sh
@@ -17,24 +17,6 @@ else
   cat src/index.html
 fi
 
-echo "dynamically fix the container-definitions_prod.json"
-cat container-definitions_prod.json.jq | jq --arg container_name $PROJECT_NAME '.[0].name=$container_name' | jq --arg image_name $IMAGE_NAME '.[0].image=$image_name' | jq --arg container_port $PROJECT_PORT '.[0].portMappings[0].containerPort=($container_port | tonumber)' | tee container-definitions_prod.json
-
-if ! [ -s container-definitions_prod.json ]
-then
-  echo "container-definitions_prod.json is empty!"
-  exit 1
-fi
-
-echo "dynamically fix the container-definitions_test.json"
-cat container-definitions_test.json.jq | jq --arg container_name $PROJECT_NAME '.[0].name=$container_name' | jq --arg image_name $IMAGE_NAME '.[0].image=$image_name' | jq --arg container_port $PROJECT_PORT '.[0].portMappings[0].containerPort=($container_port | tonumber)' | tee container-definitions_test.json
-
-if ! [ -s container-definitions_test.json ]
-then
-  echo "container-definitions_test.json is empty!"
-  exit 1
-fi
-
 echo "dynamically configuring the services.js"
 sed -i -e "s/replacethiswithcurrentenvironment/$CURRENT_ENV/g" src/static/js/services.js
 
