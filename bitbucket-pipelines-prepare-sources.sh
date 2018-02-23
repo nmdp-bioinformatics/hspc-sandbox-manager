f02b1f1cfb434efb36592b803b150ff65fbb32a4
Travis Cummings <travis@interopion.com>
2018-02-22 22:37:51 -0700

    New pipeline

--- bitbucket-pipelines-prepare-sources.sh
+++ bitbucket-pipelines-prepare-sources.sh
@@ -2,14 +2,15 @@
 
 set -e
 
-
+[[ -z "$1" ]] && { echo "usage: $0 {project_version} {test|prod}"; exit 1; }
+[[ -z "$2" ]] && { echo "usage: $0 {project_version} {test|prod}"; exit 1; }
 
 echo "starting prepare_build.sh..."
-echo "PROJECT_VERSION: $PROJECT_VERSION"
-echo "CURRENT_ENV: $CURRENT_ENV"
+echo "PROJECT_VERSION: $1"
+echo "CURRENT_ENV: $2"
 
 echo "dynamically fix the JavaScript references to bypass cache on new deployments"
-sed -E -i -e "s/.js\?r=[0-9.]+(-SNAPSHOT|-latest)?/.js\?r=$PROJECT_VERSION/g" src/index.html
+sed -E -i -e "s/.js\?r=[0-9.]+(-SNAPSHOT|-latest)?/.js\?r=$1/g" src/index.html
 if ! [ -s src/index.html ]
 then
   echo "src/index.html is empty!"
@@ -19,7 +20,7 @@ else
 fi
 
 echo "dynamically configuring the services.js"
-sed -i -e "s/replacethiswithcurrentenvironment/$CURRENT_ENV/g" src/static/js/services.js
+sed -i -e "s/replacethiswithcurrentenvironment/$2/g" src/static/js/services.js
 
 echo "active_env:"
 cat ./src/static/js/services.js | grep "var active_env ="
