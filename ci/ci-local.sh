a449ca9aa5b7ba99515b1205c15455f128265713
Travis Cummings <travis@interopion.com>
2018-02-26 02:53:13 -0700

    new pipeline

--- ci/ci-local.sh
+++ ci/ci-local.sh
@@ -3,13 +3,11 @@
 set -x
 
 # set values provided at build time
-export ACTIVE_ENV=test
-export PROJECT_NAME=sandbox-manager
-export TARGET_AWS_CLUSTER=hspc-test
+export TARGET_ENV=test
 
 . ci-0-set-properties.sh
-
 . ci-1-prepare-sources.sh
-#. ci-2-docker-image.sh
-. ci-3-aws-task-update.sh
-. ci-4-aws-service-update.sh
+#. ci-2-build-project.sh
+#. ci-3-docker-image.sh
+#. ci-4-aws-task-update.sh
+#. ci-5-aws-service-update.sh
